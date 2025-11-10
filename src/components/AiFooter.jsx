469b5dc4f7a91c66b2313b9720681aa4e3929ec7
Ezhil-Dev <johnnydepp1853@gmail.com>
2025-11-10 16:06:33 +0530

    number-centered

--- src/components/AiFooter.jsx
+++ src/components/AiFooter.jsx
@@ -5,11 +5,12 @@ import { Phone } from 'lucide-react';
 const AiFooter = () => {
   return (
     <>
-      <footer className='w-full py-4 md:text-xl text-md bg-black text-white '>
+      <footer className='w-full py-4 md:text-xl text-md bg-black text-white md:flex items-center md:justify-around'>
         <div className=' text-center md:flex items-center md:justify-around  gap-3'>
           <a className='text-blue-400 underline flex justify-center mb-3 md:mb-0' href='https://vishnuads.com/'><span className='text-white no-underline'> For more details visit </span> -  Vishnu Ads <GoArrowUpRight /></a>
-          <div className='flex items-center gap-3'>   <Phone /> +91 9876543210</div>
+          
         </div>
+        <div className='flex items-center justify-center  gap-3'>   <Phone /> +91 9876543210</div>
 
       </footer>
 
