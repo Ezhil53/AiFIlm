93d86d6d00165c982abc834a5aabbccb295aeec5
Ezhil-Dev <johnnydepp1853@gmail.com>
2025-11-11 11:08:56 +0530

    bg-change

--- src/components/AiHero.jsx
+++ src/components/AiHero.jsx
@@ -8,7 +8,7 @@ const Ai = () => {
     <>
 
       <div className="home-banner backdrop-blur-md relative bg-black min-h-screen">
-        {/* <Hyperspeed
+        <Hyperspeed
             effectOptions={{
             onSpeedUp: () => { },
             onSlowDown: () => { },
@@ -46,10 +46,10 @@ const Ai = () => {
               sticks: 0x03B3C3,
             }
           }}
-        /> */}
-        <img src={Hero} alt=""
+        />
+        {/* <img src={Hero} alt=""
           className="absolute inset-0 w-full h-full"
-           />
+           /> */}
 
         <div className="relative z-10 max-w-7xl  mx-auto px-6 py-14 ">
           
@@ -57,28 +57,28 @@ const Ai = () => {
           <div className="text-center mb-8 space-y-1">
             <div className="inline-block">
               <div className="flex items-center justify-center gap-2 mt-20 mb-6">
-                <Sparkles className="w-6 h-6 text-white animate-pulse" />
-                <span className=" text-sm font-semibold tracking-wider uppercase border border-black/20 bg-white px-4 py-2 rounded-3xl backdrop-blur-md">
-                  Next-Gen Creative Studio
+               
+                <span className=" text-sm flex items-center justify-center gap-2 font-semibold tracking-wider uppercase border border-white/60 text-white px-4 py-2 rounded-3xl backdrop-blur-md">
+                  Next-Gen Creative Studio  <Sparkles className="w-4 h-4 animate-pulse" />
                 </span>
               </div>
             </div>
             <div className="">
               <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 leading-tight ">
-                <span className="bg-linear-to-r from-white  to-gray-100 bg-clip-text text-transparent">
+                <span className="bg-linear-to-r uppercase from-white  to-gray-100 bg-clip-text text-transparent">
                   AI-Powered Films
                 </span>
               </h1>
 
-              <p className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-100">
+              <p className="text-xl md:text-2xl lg:text-3xl font-semibold text-gray-300">
                 Human-crafted Storytelling
               </p>
             </div>
           </div>
 
           {/* Feature Cards */}
-          <div className="grid md:grid-cols-3 gap-6 max-w-5xl mx-auto">
-            <div className="group relative bg-white/5 backdrop-blur-xl rounded-2xl p-8 border border-white transition-all duration-300 hover:transform hover:scale-105">
+          {/* <div className="grid md:grid-cols-3 gap-6 max-w-5xl mx-auto">
+            <div className="group relative bg-white/5 backdrop-blur-xl rounded-2xl p-8 border border-white/50 transition-all duration-300 hover:transform hover:scale-105">
               <div className="relative">
                 <h3 className="text-3xl font-bold text-white mb-3">25+ years</h3>
                 <p className="text-gray-100 text-lg leading-relaxed">
@@ -86,7 +86,7 @@ const Ai = () => {
                 </p>
               </div>
             </div>
-            <div className="group relative bg-white/5 backdrop-blur-xl rounded-2xl p-8 border border-white transition-all duration-300 hover:transform hover:scale-105">
+            <div className="group relative bg-white/5 backdrop-blur-xl rounded-2xl p-8 border border-white/50 transition-all duration-300 hover:transform hover:scale-105">
               <div className="relative">
                 <h3 className="text-3xl font-bold text-white mb-3">Fast Execution</h3>
                 <p className="text-gray-100 text-lg leading-relaxed">
@@ -95,7 +95,7 @@ const Ai = () => {
               </div>
             </div>
 
-            <div className="group relative bg-transparent backdrop-blur-md rounded-2xl p-8 border border-white transition-all duration-300 hover:transform hover:scale-105">
+            <div className="group relative bg-transparent backdrop-blur-md rounded-2xl p-8 border border-white/50 transition-all duration-300 hover:transform hover:scale-105">
               <div className="relative">
                 <h3 className="text-3xl font-bold text-white mb-3">Save Lakhs</h3>
                 <p className="text-gray-100 text-lg leading-relaxed">
@@ -103,11 +103,11 @@ const Ai = () => {
                 </p>
               </div>
             </div>
-          </div>
+          </div> */}
 
           {/* CTA Button */}
-          <div className="text-center mt-8">
-            <button className="group relative inline-flex items-center gap-3 text-black   border border-white  px-10 py-5 rounded-full text-lg font-semibold bg-white transition-all duration-300 hover:transform hover:scale-105 shadow-lg">
+          <div className="text-center mt-12">
+            <button className="group relative inline-flex items-center gap-3 text-black   border border-white  px-8 py-4 rounded-full text-lg font-semibold bg-white transition-all duration-300 hover:transform hover:scale-105 shadow-lg">
               <span>Start Your Project</span>
               <svg className="w-5 h-5 group-hover:translate-x-1 transition-transform" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7l5 5m0 0l-5 5m5-5H6" />
